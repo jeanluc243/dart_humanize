63b19ee916789e69bccd101002576f31f36d021a
Jeanluc Kabulu <jeanluc.kabulu@gmail.com>
2019-10-26 05:50:35 +0200

    seperate files and structure it

--- lib/dart_humanize.dart
+++ lib/dart_humanize.dart
@@ -1,7 +1,9 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+/// 
 library dart_humanize;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/dart_humanize_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
